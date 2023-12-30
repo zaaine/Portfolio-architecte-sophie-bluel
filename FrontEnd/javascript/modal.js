23ba0af0974f3aa5e1eda25c904971f60048afed
aziz.zaaine <zaaine@live.fr>
2023-12-30 19:28:45 +0100

    Ajout gallery dans la modal

--- FrontEnd/javascript/modal.js
+++ FrontEnd/javascript/modal.js
@@ -1,21 +1,54 @@
-// gestion de la boite de dialogue modal
+// gestion de la boite de dialogue modal (ouvrir et fermer)
 
+function modal() {
+  const openModal = document.querySelector(".open-modal");
+  openModal.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "block";
+  });
 
-function modal (){
-    const openModal = document.querySelector(".open-modal")
-    openModal.addEventListener("click", ()=> {
-    const modal = document.querySelector(".modal")    
-    modal.style.display = "block"
+  const modalClose = document.querySelector(".modalClose");
+  modalClose.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "none";
+  });
 
-})
-
-const modalClose = document.querySelector(".modalClose")
-modalClose.addEventListener("click", () => {
-    const modal = document.querySelector(".modal")   
-    modal.style.display = "none"
-})
-
-
+  // rajouter un code pour fermer la modal lorsqu'on click à l'exterieur
 }
 
-modal ()
+modal();
+
+// fonction pour insérer les photos dans la gallery-modal
+
+function createModalGallery() {
+    fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      return response.json();
+    })
+    .then((mesprojets) => {
+
+      const galleryModal = document.querySelector(".contenairGallery");
+      galleryModal.classList.add = "contenairGallery";
+    
+
+      for (const projets of mesprojets) {
+        const projet = document.createElement("figure");
+        
+        const imageElement = document.createElement("img");
+        imageElement.src = projets.imageUrl;
+        
+        projet.setAttribute("data-id", projets.id);
+        projet.setAttribute("category-id", projets.categoryId);
+        
+        galleryModal.appendChild(projet);
+        projet.appendChild(imageElement);
+        imageElement.classList.toggle = ("photoModal");
+    }
+
+ 
+        
+    });
+      
+}
+
+createModalGallery();
