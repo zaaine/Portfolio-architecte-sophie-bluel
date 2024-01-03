1a6f69f03995d6e3f62ef59f875d1ef7c0f599c8
aziz.zaaine <zaaine@live.fr>
2024-01-03 13:14:24 +0100

    Navigation entre les modales ok. Ajout de photo possible et inputs fonctionnelles. correction des conflits ouvertures modales

--- FrontEnd/javascript/modal.js
+++ FrontEnd/javascript/modal.js
@@ -1,33 +1,37 @@
-// gestion de la boite de dialogue modal (ouvrir et fermer)
+// gestion de la boite de dialogue modal 1 (ouvrir et fermer)
 
 function modal() {
   const openModal = document.querySelector(".open-modal");
   openModal.addEventListener("click", () => {
     const modal = document.querySelector(".modal");
     modal.style.display = "block";
-});
+    
 
     const modalClose = document.querySelector(".modalClose");
     modalClose.addEventListener("click", (event) => {
-        const modal = document.querySelector(".modal");
-        modal.style.display = "none";
-        event.preventDefault();
+      const modal = document.querySelector(".modal");
+      modal.style.display = "none";
+      event.preventDefault();
     });
-   
-    const modal = document.querySelector(".modal");
-        modal.addEventListener("click", (event) => {
-        modal.style.display = "none";
-        event.preventDefault();
-        console.log("window");
-    
+
+ 
   });
-  
+
+  const modal = document.querySelector(".modal");
+  modal.addEventListener("click", () => {
+    modal.style.display = "none";
+    modalContent.stopPropagation();
+  });
+
+  const modalContent = document.querySelector(".modal-content");
+  modalContent.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
 }
 
 //   la modale doit se fermer au click sur l'exterieur de la modal
 
-modal()
-
+modal();
 
 // closeModal()
 
@@ -88,7 +92,6 @@ function deleteWork(workId) {
         `figure[data-id = "${workId}"]`
       );
       projetReset.style.display = "none";
-     
     } else if (response.status === 401) {
       throw new Error("Unauthorized");
     } else if (response.status === 500) {
@@ -97,3 +100,48 @@ function deleteWork(workId) {
     return false;
   });
 }
+
+// Gestion de la modale 2 "Ajout de Travaux"
+
+// navigation entre les modales
+
+function navigationModales() {
+  
+  const btnAddPhoto = document.querySelector(".btnAddModal");
+
+  btnAddPhoto.addEventListener("click", (event) => {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "none";
+    event.preventDefault();
+
+    const openmodal2 = document.querySelector(".openmodal2");
+    openmodal2.style.display = "block";
+    console.log("add");
+  });
+
+  const btnReturnGallery= document.querySelector(".returnGallery")
+  btnReturnGallery.addEventListener("click", ()=>{
+
+    const openmodal2 = document.querySelector(".openmodal2");
+    openmodal2.style.display = "none";
+    console.log("add");
+
+    const modal = document.querySelector(".modal");
+    modal.style.display = "block";
+  })
+
+
+  const modal2 = document.querySelector(".openmodal2");
+  modal2.addEventListener("click", () => {
+    modal2.style.display = "none";
+    
+  });
+
+  const modal2Containt = document.querySelector(".modal2_Containt");
+  modal2Containt.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+
+}
+
+navigationModales()
